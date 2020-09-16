73477e1d857ee1cedc78cf8a5815c2e5c7132a2d
Jason Weyland <jason.weyland13@gmail.com>
2020-09-16 15:53:20 -0400

    first commit

---
+++ components/mainComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Projects from "./projectsComponent";
+
+class Main extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <Projects />;
+  }
+}
+
+export default Main;
