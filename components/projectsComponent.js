e00b89ca9e76b5f3893e035d06e63b4cf90d1a5c
Jason Weyland <jason.weyland13@gmail.com>
2020-10-21 01:25:15 -0400

    Update project post error handling

--- components/projectsComponent.js
+++ components/projectsComponent.js
@@ -47,7 +47,7 @@ class Projects extends Component {
     return (
       <View style={{ flex: 1 }}>
         <RenderContent projects={this.props.projects.projects}/>
-        <TouchableOpacity style={styles.TouchableOpacityStyle} onPress={() => navigate('Create Project')}>
+        <TouchableOpacity style={styles.TouchableOpacityStyle} onPress={() => navigate('CreateProject')}>
           <Icon name={"plus"} type={"font-awesome"} raised reverse color="#00ced1" style={styles.FloatingButtonStyle} />
         </TouchableOpacity>
       </View>
