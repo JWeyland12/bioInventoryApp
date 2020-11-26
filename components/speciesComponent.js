ca4894066693266764d4d81e8fceca4b8f217976
Jason Weyland <jason.weyland13@gmail.com>
2020-11-25 21:25:18 -0500

    species floating button

--- components/speciesComponent.js
+++ components/speciesComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import {View, FlatList, Text} from 'react-native';
-import { ListItem } from "react-native-elements";
+import {View, FlatList, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import { ListItem, Icon } from "react-native-elements";
 import {connect} from 'react-redux';
 
 const mapStateToProps = state => {
@@ -31,9 +31,30 @@ class SpeciesList extends Component {
     return (
       <View style={{flex: 1}}>
         <FlatSpeciesList speciesAlpha={speciesAlpha}/>
+        <TouchableOpacity style={styles.TouchableOpacityStyle}>
+          <Icon name={"plus"} type={"font-awesome"} raised reverse color="#00ced1" style={styles.FloatingButtonStyle} />
+        </TouchableOpacity>
       </View>
     );
   }
 }
 
+
 export default connect(mapStateToProps)(SpeciesList);
+
+const styles = StyleSheet.create({
+  TouchableOpacityStyle: {
+    position: "absolute",
+    width: 50,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    right: 30,
+    bottom: 30,
+  },
+  FloatingButtonStyle: {
+    resizeMode: "contain",
+    width: 50,
+    height: 50,
+  }
+});
